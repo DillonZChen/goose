a86b1b71e1f7f7154df9eafc64188eb1534fc61a
Dillon <u6942650@anu.edu.au>
2023-06-17 11:21:02 +1000

    changes

--- learner/scripts/submit_train_domain.sh
+++ learner/scripts/submit_train_domain.sh
@@ -1,6 +1,11 @@
 DOMAINS="blocks ferry gripper n-puzzle sokoban spanner visitall visitsome"
 
-for domain in $DOMAINS
+for domain in blocks ferry gripper n-puzzle sokoban spanner
 do
-  sbatch --job-name=${domain} --output=logs/cluster1_train_${domain}.log scripts/cluster1_job_any.sh "python3 scripts/train_domain.py ${domain}"
+  sbatch --job-name=${domain} --output=logs/cluster1_train_${domain}.log scripts/cluster1_job_any "python3 scripts/train_domain.py ${domain}"
+done
+
+for domain in visitall visitsome
+do
+  sbatch --job-name=${domain} --output=logs/cluster1_train_${domain}.log scripts/cluster1_job_planopt "python3 scripts/train_domain.py ${domain}"
 done
--- learner/util/stats.py
+++ learner/util/stats.py
@@ -58,12 +58,12 @@ def get_y_stats(dataset):
 
   ys = np.array(ys)
   os.makedirs("plots/", exist_ok=True)
-  plt.hist(ys, bins=round(np.max(ys) + 1),
-           range=(0, round(np.max(ys) + 1)))
-  plt.xlim(left=0)
-  # plt.title('y distribution')
-  plt.savefig('plots/y_distribution.pdf', bbox_inches="tight")
-  plt.clf()
+  # plt.hist(ys, bins=round(np.max(ys) + 1),
+  #          range=(0, round(np.max(ys) + 1)))
+  # plt.xlim(left=0)
+  # # plt.title('y distribution')
+  # plt.savefig('plots/y_distribution.pdf', bbox_inches="tight")
+  # plt.clf()
 
   return ys
 
