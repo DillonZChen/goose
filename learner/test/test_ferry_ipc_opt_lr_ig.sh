e1c76e67a0b5261a4692e23d451b93801b4a7363
dillon <dillon.chen1@gmail.com>
2023-10-30 14:08:00 +0100

    kernel backup

--- learner/test/test_ferry_ipc_opt_lr_ig.sh
+++ learner/test/test_ferry_ipc_opt_lr_ig.sh
@@ -2,8 +2,11 @@ r=ig
 
 m=linear-svr_${r}_ipc2023-learning-ferry_wl_1_0
 
-python3 scripts_kernel/generate_graphs_kernel.py $r --regenerate -d ipc2023-learning-ferry
-python3 train_kernel.py -k wl -l 1 -r $r -d ipc2023-learning-ferry -m linear-svr -p 0 --save-file $m
+# python3 scripts_kernel/generate_graphs_kernel.py $r --regenerate -d ipc2023-learning-ferry
+domain_pddl=../benchmarks/ipc2023-learning-benchmarks/ferry/domain.pddl
+tasks_dir=../benchmarks/ipc2023-learning-benchmarks/ferry/training/easy
+plans_dir=../benchmarks/ipc2023-learning-benchmarks/ferry/training_plans
+python3 train_kernel.py $domain_pddl $tasks_dir $plans_dir -k wl -l 1 -r $r -d ipc2023-learning-ferry -m linear-svr -p 0 --save-file $m
 
 df=../benchmarks/ipc2023-learning-benchmarks/ferry/domain.pddl
 pf=../benchmarks/ipc2023-learning-benchmarks/ferry/training/easy/p99.pddl
