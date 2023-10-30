f88dde96bf3f725c0eb4a525d760e614277e884a
dillon <dillon.chen1@gmail.com>
2023-10-30 16:12:21 +0100

    backup some scripts

--- learner/scripts_kernel/collect_gadi_test.sh
+++ learner/scripts_kernel/collect_gadi_test.sh
@@ -1,2 +1,6 @@
-mkdir -p logs/test_ipc2023
-scp -r gadi:~/goose/learner/logs_kernel/test_ipc2023/ logs/
+mkdir -p logs/opt_ipc2023_test
+ssh gadi "zip -r opt_ipc2023_test.zip goose/learner/logs_kernel/opt_ipc2023_test"
+scp gadi:~/opt_ipc2023_test.zip .
+unzip opt_ipc2023_test
+mv goose/learner/logs_kernel/opt_ipc2023_test/* logs/opt_ipc2023_test
+rm -rf goose/learner/logs_kernel/opt_ipc2023_test/
