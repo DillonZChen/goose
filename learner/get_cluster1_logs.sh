4c3a9fc0ee50739bb037cbfcad17bea866ddd20d
dillon <dillon.chen1@gmail.com>
2023-11-26 15:50:15 +0100

    grounded graphs fix

--- learner/get_cluster1_logs.sh
+++ learner/get_cluster1_logs.sh
@@ -3,3 +3,4 @@ ZIP_FILE_NAME=zipped_aaai24_logs.tar.gz
 ssh cluster1 "cd goose/learner;tar -czvf $ZIP_FILE_NAME aaai24_logs/"
 scp cluster1:~/goose/learner/$ZIP_FILE_NAME .
 tar -xzvf $ZIP_FILE_NAME
+rm $ZIP_FILE_NAME
