bf0122a9287e4e0de4a4e20a9bddb4554f5cfc7b
Dillon <dillon.chen1@gmail.com>
2025-01-17 16:15:50 +1100

    backup

--- experiments/plan.sh
+++ experiments/plan.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-PROBLEM=0_27
 DOMAIN=blocksworld
 f=wl
 L=4
-p=collapse-layer
+p=collapse-all
 d=plan
-save_file=experiments/_short/model/${DOMAIN}_${f}_${L}_${p}_${d}.model
-log_file=experiments/_short/plan/${DOMAIN}_${f}_${L}_${p}_${d}.log
+multiset_hash=1
+facts=fd
+save_file=experiments/_short/model/${DOMAIN}_${f}_${L}_${p}_${d}_${multiset_hash}_${facts}.model
+log_file=experiments/_short/plan/${DOMAIN}_${f}_${L}_${p}_${d}_${multiset_hash}_${facts}.log
 mkdir -p experiments/_short/model
 mkdir -p experiments/_short/plan
+
+PROBLEM=1_01
 
 domain_pddl=benchmarks/ipc23lt/$DOMAIN/domain.pddl
 problem_pddl=benchmarks/ipc23lt/$DOMAIN/testing/p${PROBLEM}.pddl
--- experiments/train.sh
+++ experiments/train.sh
@@ -3,12 +3,12 @@
 DOMAIN=blocksworld
 f=wl
 L=4
-p=collapse-layer
+p=collapse-all
 d=plan
-multiset_hash=0
-facts=nostatic
-save_file=experiments/_short/model/${DOMAIN}_${f}_${L}_${p}_${d}.model
-log_file=experiments/_short/train/${DOMAIN}_${f}_${L}_${p}_${d}.log
+multiset_hash=1
+facts=fd
+save_file=experiments/_short/model/${DOMAIN}_${f}_${L}_${p}_${d}_${multiset_hash}_${facts}.model
+log_file=experiments/_short/train/${DOMAIN}_${f}_${L}_${p}_${d}_${multiset_hash}_${facts}.log
 mkdir -p experiments/_short/model
 mkdir -p experiments/_short/train
 
