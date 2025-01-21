d035f9be27e35d208c57cb65450b31c1c998050c
Dillon <dillon.chen1@gmail.com>
2025-01-21 11:11:43 +1100

    update config

--- experiments/plan.sh
+++ experiments/plan.sh
@@ -28,48 +28,16 @@ python3 plan.py $domain_pddl $problem_pddl $save_file | tee $log_file
 
 # wl, 4, none pruning
 # blocksworld 1_12
-# [t=5.922040s, 76116 KB] Plan length: 334 step(s).
-# [t=5.922040s, 76116 KB] Plan cost: 334
-# [t=5.922040s, 76116 KB] Expanded 1193 state(s).
-# [t=5.922040s, 76116 KB] Reopened 0 state(s).
-# [t=5.922040s, 76116 KB] Evaluated 29499 state(s).
-# [t=5.922040s, 76116 KB] Evaluations: 29499
-# [t=5.922040s, 76116 KB] Generated 31064 state(s).
-# [t=5.922040s, 76116 KB] Dead ends: 0 state(s).
-# [t=5.922040s, 76116 KB] Number of registered states: 29499
-# [t=5.922040s, 76116 KB] Int hash set load factor: 29499/65536 = 0.450119
-# [t=5.922040s, 76116 KB] Int hash set resizes: 16
-# [t=5.922040s, 76116 KB] Search time: 5.837055s
-# [t=5.922040s, 76116 KB] Total time: 5.922040s
-
-# wl, 4, collapse-all pruning
-# blocksworld 1_12
-# [t=3.014065s, 73820 KB] Plan length: 316 step(s).
-# [t=3.014065s, 73820 KB] Plan cost: 316
-# [t=3.014065s, 73820 KB] Expanded 682 state(s).
-# [t=3.014065s, 73820 KB] Reopened 0 state(s).
-# [t=3.014065s, 73820 KB] Evaluated 17777 state(s).
-# [t=3.014065s, 73820 KB] Evaluations: 17777
-# [t=3.014065s, 73820 KB] Generated 18465 state(s).
-# [t=3.014065s, 73820 KB] Dead ends: 0 state(s).
-# [t=3.014065s, 73820 KB] Number of registered states: 17777
-# [t=3.014065s, 73820 KB] Int hash set load factor: 17777/32768 = 0.542511
-# [t=3.014065s, 73820 KB] Int hash set resizes: 15
-# [t=3.014065s, 73820 KB] Search time: 2.923480s
-# [t=3.014065s, 73820 KB] Total time: 3.014065s
-
-# wl, 4, collapse-layer pruning
-# blocksworld 0_20
-# [t=0.044998s, 13740 KB] Plan length: 82 step(s).
-# [t=0.044998s, 13740 KB] Plan cost: 82
-# [t=0.044998s, 13740 KB] Expanded 167 state(s).
-# [t=0.044998s, 13740 KB] Reopened 0 state(s).
-# [t=0.044998s, 13740 KB] Evaluated 1053 state(s).
-# [t=0.044998s, 13740 KB] Evaluations: 1053
-# [t=0.044998s, 13740 KB] Generated 1220 state(s).
-# [t=0.044998s, 13740 KB] Dead ends: 0 state(s).
-# [t=0.044998s, 13740 KB] Number of registered states: 1053
-# [t=0.044998s, 13740 KB] Int hash set load factor: 1053/2048 = 0.514160
-# [t=0.044998s, 13740 KB] Int hash set resizes: 11
-# [t=0.044998s, 13740 KB] Search time: 0.036000s
-# [t=0.044998s, 13740 KB] Total time: 0.044998s
+# [t=6.038954s, 76116 KB] Plan length: 334 step(s).
+# [t=6.038954s, 76116 KB] Plan cost: 334
+# [t=6.038954s, 76116 KB] Expanded 1193 state(s).
+# [t=6.038954s, 76116 KB] Reopened 0 state(s).
+# [t=6.038954s, 76116 KB] Evaluated 29499 state(s).
+# [t=6.038954s, 76116 KB] Evaluations: 29499
+# [t=6.038954s, 76116 KB] Generated 31064 state(s).
+# [t=6.038954s, 76116 KB] Dead ends: 0 state(s).
+# [t=6.038954s, 76116 KB] Number of registered states: 29499
+# [t=6.038954s, 76116 KB] Int hash set load factor: 29499/65536 = 0.450119
+# [t=6.038954s, 76116 KB] Int hash set resizes: 16
+# [t=6.038954s, 76116 KB] Search time: 5.952955s
+# [t=6.038954s, 76116 KB] Total time: 6.038954s
