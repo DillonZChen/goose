1e772170b4a862b39d14274e3d95feb09fc75261
Dillon <dillon.chen1@gmail.com>
2025-07-01 16:35:54 +0200

    [wip] q

--- learning/dataset/pyg_loader.py
+++ learning/dataset/pyg_loader.py
@@ -6,9 +6,55 @@ from torch_geometric.data import Data
 from torch_geometric.loader import DataLoader
 
 from learning.dataset.creator.classic_labelled_dataset_creator import LabelledDataset
+from learning.predictor.neural_network.policy_type import PolicyType
 from wlplan.data import DomainDataset, ProblemDataset
 from wlplan.graph_generator import GraphGenerator
 from wlplan.planning import Domain
+
+
+def _get_value_function_dataset(domain: Domain, dataset: LabelledDataset) -> DomainDataset:
+    problem_dataset_list = []
+    labels = []
+    for data in dataset:
+        problem = data.problem
+        states = []
+        for state_and_successors in data.states_and_successors_labelled:
+            states.append(state_and_successors.state)  # s
+            labels.append(state_and_successors.value)  # Q(s)
+            for successors in state_and_successors.successors_labelled:
+                succ = successors.successor_state  # s
+                value = successors.value  # Q(s)
+                if value is not None:
+                    states.append(succ)
+                    labels.append(value)
+
+        problem_dataset_list.append(ProblemDataset(problem=problem, states=states))
+    domain_dataset = DomainDataset(domain=domain, data=problem_dataset_list)
+    return domain_dataset
+
+
+def _get_quality_function_dataset(domain: Domain, dataset: LabelledDataset) -> DomainDataset:
+    problem_dataset_list = []
+    labels = []
+    for data in dataset:
+        problem = data.problem
+        states = []
+        actions = []
+        for state_and_successors in data.states_and_successors_labelled:
+            state = state_and_successors.state  # s
+            value = state_and_successors.value  # do nothing with the current value
+            for successors in state_and_successors.successors_labelled:
+                succ_state = successors.successor_state  # do nothing with the succ_state
+                succ_value = successors.value  # use the succ value as Q(s, a)
+                action = successors.action  # a
+                if succ_value is not None:
+                    states.append(state)
+                    actions.append(action)
+                    labels.append(succ_value)
+
+        problem_dataset_list.append(ProblemDataset(problem=problem, states=states, actions=actions))
+    domain_dataset = DomainDataset(domain=domain, data=problem_dataset_list)
+    return domain_dataset
 
 
 def get_data_loaders(
@@ -19,23 +65,13 @@ def get_data_loaders(
     policy_type: str,
 ) -> tuple[DataLoader, DataLoader]:
 
-    problem_dataset_list = []
-    labels = []
-    for data in dataset:
-        problem = data.problem
-        states = []
-        for state_and_successors in data.states_and_successors_labelled:
-            states.append(state_and_successors.state)
-            labels.append(state_and_successors.value)
-            for successors in state_and_successors.successors_labelled:
-                succ = successors.successor_state
-                value = successors.value
-                if value is not None:
-                    states.append(succ)
-                    labels.append(value)
-
-        problem_dataset_list.append(ProblemDataset(problem=problem, states=states))
-    domain_dataset = DomainDataset(domain=domain, data=problem_dataset_list)
+    match policy_type:
+        case PolicyType.VALUE_FUNCTION.value:
+            domain_dataset = _get_value_function_dataset(domain, dataset)
+        case PolicyType.QUALITY_FUNCTION.value:
+            domain_dataset = _get_quality_function_dataset(domain, dataset)
+        case _:
+            raise ValueError(f"Unknown policy type: {policy_type}")
 
     graphs = graph_generator.to_graphs(domain_dataset)
 
--- learning/predictor/neural_network/policy_type.py
+++ learning/predictor/neural_network/policy_type.py
@@ -2,9 +2,13 @@ from enum import Enum
 
 
 class PolicyType(Enum):
+    # Predict V(s) functions
     VALUE_FUNCTION = "v"
+    # Predict Q(s, a) functions; Q for quality https://en.wikipedia.org/wiki/Q-learning
     QUALITY_FUNCTION = "q"
+    # Predict advantage function: A(s, a) = Q(s, a) - V(s)
     ADVANTAGE_FUNCTION = "a"
+    # Predict distribution over actions: P(a | s)
     DISTRIBUTION = "d"
 
 
