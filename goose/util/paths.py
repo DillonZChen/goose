9be7fcccad509c142834b054f324b8e53c8a36b4
Dillon Chen <dillon.chen1@protonmail.com>
2026-01-05 10:10:42 +1100

    [fix] fix binary path

--- goose/util/paths.py
+++ goose/util/paths.py
@@ -5,6 +5,6 @@ _CUR_DIR = os.path.dirname(os.path.abspath(__file__))
 GOOSE_ROOT_DIR = os.path.normpath(f"{_CUR_DIR}/..")
 PLANNERS_DIR = os.path.normpath(f"{GOOSE_ROOT_DIR}/../ext/planners")
 DOWNWARD_SCRIPT = f"{PLANNERS_DIR}/scorpion/fast-downward.py"
-DOWNWARD_BIN = f"{PLANNERS_DIR}/scorpion/fast-downward/builds/release"
+DOWNWARD_BIN = f"{PLANNERS_DIR}/scorpion/builds/release"
 POWERLIFTED_SCRIPT = f"{PLANNERS_DIR}/powerlifted/powerlifted.py"
 POWERLIFTED_BIN = f"{PLANNERS_DIR}/powerlifted/builds/release"
